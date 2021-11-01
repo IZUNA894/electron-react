b8f666350236eac1e09873c84bb7ce55c97202c8
IZUNA894 <jainsanyam1261@gmail.com>
2021-11-01 18:08:10 +0530

    base commit

---
+++ build/precache-manifest.8e33944cbebb53bb4c369ecd2d96d953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98c4963d946bec2d7f715e4b0457e47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7251c7d469acd5a8cba",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "084ddda3f6efaf39310b",
+    "url": "./static/js/2.2d9196bf.chunk.js"
+  },
+  {
+    "revision": "479235235e4cb9c7901a76659caeadae",
+    "url": "./static/js/2.2d9196bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7251c7d469acd5a8cba",
+    "url": "./static/js/main.9f869e15.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
